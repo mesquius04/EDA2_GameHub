3b9fdd3ff6d6bfe3948677bddffcc99c8c393501
RomanAtanasiu <131767614+RomanAtanasiu@users.noreply.github.com>
2023-06-11 18:42:34 +0200

    Update services.c

--- services.c
+++ services.c
@@ -5,7 +5,7 @@
 #include <stdio.h>
 #include <CommCtrl.h>
 #include "main.h"
-#include <time.h>
+
 void print_console_users(User* list){
     while(list != NULL){
         printf("\n%s",list->user);
@@ -33,10 +33,3 @@ int validar_email(char *mail) { //un mail válido tiene esta estructura ______@g
 }
 
 
-//genera un número eneteo aleatotio con un mínimo (incluido) y un máximo (sin incluir) de rango
-int entero_aleatorio_fronteras(int min, int max){
-    srand(time(NULL));
-    int num = rand();
-    //se calcula el resto del número del rango y se le añade el mínimo, por lo tanto siempre estará en el rango
-    return num%(max-min) + min;
-}
